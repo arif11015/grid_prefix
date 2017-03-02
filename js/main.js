0567ed52b16c864901ced5d4856e09d7a02dc531
Arif Rahman <xossarif@gmail.com>
2017-03-02 21:13:02 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+jQuery(document).ready(function($){
+/* nav bar */
+    var scrollSpeed = 750;
+    var scrollOffset = 50;
+    var easing = 'swing';
+
+    $('#navbar-top .navbar-default ul.nav').onePageNav({
+        currentClass: 'active',
+        changeHash: false,
+        scrollSpeed: scrollSpeed,
+        scrollOffset: scrollOffset,
+        scrollThreshold: 0.5,
+        filter: ':not(.external)',
+        easing: easing
+    });
+
+    $('.nav-external').click(function (e) {
+        e.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr("href")).offset().top - scrollOffset
+        }, scrollSpeed, easing);
+    });
+
+    $('#navbar-top .navbar-default').affix({
+        offset: {
+            top: $('#home').height()
+        }
+    });  
+  
+  
+  
+
+	
+	
+	
+});
+
+
+
+
